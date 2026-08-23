c001ef6e5371c52e5ab809e74d85a0240227e822
developer-akashatrai <gamertech275@gmail.com>
2026-08-23 11:11:35 +0530

    Day-2 of react

---
+++ Day-2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  console.log("hello")
+  return (
+    
+    <div className =" text-3xl m-10"  >
+      <h2>Hello I am Akshat Rai from India</h2>
+      <h2>0</h2>
+      <button className="h-10 w-50 bg-amber-600 rounded flex flex-col items-center
+      justify-items-center cursor-pointer ">Counter</button>
+
+    </div>
+  )
+}
+
+export default App
